a4769a9da98b79f384078864378ccc8dad36572c
SaharAlassaf <Sahar.Hussain.elq.js@tuwaiq.edu.sa>
2021-11-30 21:25:57 +0300

    task

--- routers/controllers/task.js
+++ routers/controllers/task.js
@@ -1,6 +1,57 @@
 const userModel = require("./../../db/models/user");
 const taskModel = require("./../../db/models/task");
 
+//Sow all tasks for Admin
+const tasks = (req, res) => {
+  taskModel
+    .find({})
+    .populate("byUser")
+    .then((result) => {
+      res.status(201).send(result);
+    })
+    .catch((err) => {
+      res.status(400).send(err);
+    });
+};
+
+//Show task by id
+const task = (req, res) => {
+  const { id } = req.params;
+  taskModel
+    .findById(id)
+    .where("isDel")
+    .equals(false)
+    .exec()
+    .then((result) => {
+      if (result) {
+        res.status(201).send(result);
+      } else {
+        res.status(404).send("Task is not exist");
+      }
+    })
+    .catch((err) => {
+      res.status(400).send(err);
+    });
+};
+
+//Show deleted task for Admin
+const delatedTask = (req, res) => {
+  taskModel
+    .find({ isDel: { $eq: false } })
+    .exec()
+    .then((result) => {
+      if (result) {
+        res.status(201).send(result);
+      } else {
+        res.status(404).send("There is no task");
+      }
+    })
+    .catch((err) => {
+      res.status(400).send(err);
+    });
+};
+
+//Add task
 const createTask = (req, res) => {
   const { name } = req.body;
 
@@ -19,4 +70,44 @@ const createTask = (req, res) => {
     });
 };
 
-module.exports = { createTask };
+// update task
+const updateTask = (req, res) => {
+  const { id } = req.params;
+  const { name, isDel } = req.body;
+
+  taskModel
+    .findByIdAndUpdate(id, { name, isDel }, { new: true })
+    .exec()
+    .then((result) => {
+      if (result) {
+        res.status(201).send(result);
+      } else {
+        res.status(404).send("Failed update");
+      }
+    })
+    .catch((err) => {
+      res.status(400).send(err);
+    });
+};
+
+// delete task
+const deleteTask = (req, res) => {
+    const { id } = req.params;
+    const { name, isDel } = req.body;
+  
+    taskModel
+      .findOneAndUpdate({id, isDel: { $eq: false }}, { isDel: true }, { new: true })
+      .exec()
+      .then((result) => {
+        if (result) {
+          res.status(201).send(result);
+        } else {
+          res.status(404).send("It's deleted");
+        }
+      })
+      .catch((err) => {
+        res.status(400).send(err);
+      });
+  };
+
+module.exports = { tasks, createTask, task, delatedTask, updateTask, deleteTask };
--- routers/routes/task.js
+++ routers/routes/task.js
@@ -1,9 +1,17 @@
 const express = require("express");
-const { createTask } = require("../controllers/task");
+const { tasks, task, createTask, delatedTask, updateTask, deleteTask } = require("../controllers/task");
 const authentication = require("./../middlewares/authentication");
+const authorization = require("./../middlewares/authorization");
 
 const taskRouter = express.Router();
 
+taskRouter.get("/", authentication, authorization, tasks); //just Admin
+taskRouter.get("/delatedTask", authentication,authorization, delatedTask); //just Admin
 taskRouter.post("/createTask", authentication, createTask);
+
+taskRouter.get("/:id", authentication, task);
+taskRouter.put("/updateTask/:id", authentication, updateTask);
+taskRouter.delete("/deleteTask/:id", authentication, deleteTask);
+
 
 module.exports = taskRouter;
